04679fbb254c5a304d6c7c76a46629e0daddcc67
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-03-16 16:43:35 +0200

    Home banner

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import video from '../../assets/video.mp4';
+import aeroplane from '../../assets/flying.png';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home flex container">
+      <div className="mainText">
+        <h1>Create Ever-lasting Memories With Us</h1>
+      </div>
+
+      <div className="homeImages flex">
+        <div className="videoDiv">
+          <video
+            src={video}
+            autoPlay
+            muted
+            loop
+            className="video"
+          ></video>
+        </div>
+
+        <img src={aeroplane} className="plane" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
